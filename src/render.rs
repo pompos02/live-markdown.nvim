275aea578f17cb1a197533ede6511a4b8f8b74d6
pompos02 <yiannis.caravellas@gmail.com>
2026-02-14 14:38:11 +0200

    feat: block styles rendered

--- src/render.rs
+++ src/render.rs
@@ -134,11 +134,16 @@ fn render_start_tag(
             out.push_str(&line.to_string());
             out.push('"');
             if let Some(kind) = kind {
+                let kind_name = block_quote_kind_name(kind);
                 out.push_str(" data-alert=\"");
-                out.push_str(block_quote_kind_name(kind));
-                out.push('"');
+                out.push_str(kind_name);
+                out.push_str("\" class=\"markdown-alert markdown-alert-");
+                out.push_str(kind_name);
+                out.push_str("\">");
+                render_alert_title(out, kind);
+            } else {
+                out.push('>');
             }
-            out.push('>');
         }
         Tag::CodeBlock(kind) => {
             out.push_str("<pre data-line=\"");
@@ -566,6 +571,44 @@ fn block_quote_kind_name(kind: BlockQuoteKind) -> &'static str {
     }
 }
 
+fn block_quote_kind_title(kind: BlockQuoteKind) -> &'static str {
+    match kind {
+        BlockQuoteKind::Note => "Note",
+        BlockQuoteKind::Tip => "Tip",
+        BlockQuoteKind::Important => "Important",
+        BlockQuoteKind::Warning => "Warning",
+        BlockQuoteKind::Caution => "Caution",
+    }
+}
+
+fn block_quote_kind_icon_path(kind: BlockQuoteKind) -> &'static str {
+    match kind {
+        BlockQuoteKind::Note => {
+            "M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8Zm8-6.5a6.5 6.5 0 1 0 0 13 6.5 6.5 0 0 0 0-13ZM6.5 7.75A.75.75 0 0 1 7.25 7h1a.75.75 0 0 1 .75.75v2.75h.25a.75.75 0 0 1 0 1.5h-2a.75.75 0 0 1 0-1.5h.25v-2h-.25a.75.75 0 0 1-.75-.75ZM8 6a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+        }
+        BlockQuoteKind::Tip => {
+            "M8 1.5c-2.363 0-4 1.69-4 3.75 0 .984.424 1.625.984 2.304l.214.253c.223.264.47.556.673.848.284.411.537.896.621 1.49a.75.75 0 0 1-1.484.211c-.04-.282-.163-.547-.37-.847a8.456 8.456 0 0 0-.542-.68c-.084-.1-.173-.205-.268-.32C3.201 7.75 2.5 6.766 2.5 5.25 2.5 2.31 4.863 0 8 0s5.5 2.31 5.5 5.25c0 1.516-.701 2.5-1.328 3.259-.095.115-.184.22-.268.319-.207.245-.383.453-.541.681-.208.3-.33.565-.37.847a.751.751 0 0 1-1.485-.212c.084-.593.337-1.078.621-1.489.203-.292.45-.584.673-.848.075-.088.147-.173.213-.253.561-.679.985-1.32.985-2.304 0-2.06-1.637-3.75-4-3.75ZM5.75 12h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1 0-1.5ZM6 15.25a.75.75 0 0 1 .75-.75h2.5a.75.75 0 0 1 0 1.5h-2.5a.75.75 0 0 1-.75-.75Z"
+        }
+        BlockQuoteKind::Important => {
+            "M0 1.75C0 .784.784 0 1.75 0h12.5C15.216 0 16 .784 16 1.75v9.5A1.75 1.75 0 0 1 14.25 13H8.06l-2.573 2.573A1.458 1.458 0 0 1 3 14.543V13H1.75A1.75 1.75 0 0 1 0 11.25Zm1.75-.25a.25.25 0 0 0-.25.25v9.5c0 .138.112.25.25.25h2a.75.75 0 0 1 .75.75v2.19l2.72-2.72a.749.749 0 0 1 .53-.22h6.5a.25.25 0 0 0 .25-.25v-9.5a.25.25 0 0 0-.25-.25Zm7 2.25v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 9a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+        }
+        BlockQuoteKind::Warning => {
+            "M6.457 1.047c.659-1.234 2.427-1.234 3.086 0l6.082 11.378A1.75 1.75 0 0 1 14.082 15H1.918a1.75 1.75 0 0 1-1.543-2.575Zm1.763.707a.25.25 0 0 0-.44 0L1.698 13.132a.25.25 0 0 0 .22.368h12.164a.25.25 0 0 0 .22-.368Zm.53 3.996v2.5a.75.75 0 0 1-1.5 0v-2.5a.75.75 0 0 1 1.5 0ZM9 11a1 1 0 1 1-2 0 1 1 0 0 1 2 0Z"
+        }
+        BlockQuoteKind::Caution => {
+            "M4.47.22A.749.749 0 0 1 5 0h6c.199 0 .389.079.53.22l4.25 4.25c.141.14.22.331.22.53v6a.749.749 0 0 1-.22.53l-4.25 4.25A.749.749 0 0 1 11 16H5a.749.749 0 0 1-.53-.22L.22 11.53A.749.749 0 0 1 0 11V5c0-.199.079-.389.22-.53Zm.84 1.28L1.5 5.31v5.38l3.81 3.81h5.38l3.81-3.81V5.31L10.69 1.5ZM8 4a.75.75 0 0 1 .75.75v3.5a.75.75 0 0 1-1.5 0v-3.5A.75.75 0 0 1 8 4Zm0 8a1 1 0 1 1 0-2 1 1 0 0 1 0 2Z"
+        }
+    }
+}
+
+fn render_alert_title(out: &mut String, kind: BlockQuoteKind) {
+    out.push_str("<p class=\"markdown-alert-title\"><svg class=\"octicon markdown-alert-icon\" viewBox=\"0 0 16 16\" width=\"16\" height=\"16\" aria-hidden=\"true\"><path d=\"");
+    out.push_str(block_quote_kind_icon_path(kind));
+    out.push_str("\"></path></svg>");
+    out.push_str(block_quote_kind_title(kind));
+    out.push_str("</p>");
+}
+
 fn metadata_block_kind_name(kind: MetadataBlockKind) -> &'static str {
     match kind {
         MetadataBlockKind::YamlStyle => "yaml",
@@ -648,6 +691,40 @@ mod tests {
         assert!(html.contains("<h1 data-line=\"1\" id=\"heading\">Heading</h1>"));
         assert!(html.contains("<li data-line=\"3\">one</li>"));
         assert!(html.contains("<code>code</code>"));
+    }
+
+    #[test]
+    fn renders_gfm_alert_block_quotes_with_titles_and_icons() {
+        let renderer = MarkdownRenderer::default();
+        let markdown = "> [!NOTE]\n> note body\n\n> [!TIP]\n> tip body\n\n> [!IMPORTANT]\n> important body\n\n> [!WARNING]\n> warning body\n\n> [!CAUTION]\n> caution body";
+        let html = renderer.render(markdown);
+
+        assert!(html.contains("class=\"markdown-alert markdown-alert-note\""));
+        assert!(html.contains("class=\"markdown-alert markdown-alert-tip\""));
+        assert!(html.contains("class=\"markdown-alert markdown-alert-important\""));
+        assert!(html.contains("class=\"markdown-alert markdown-alert-warning\""));
+        assert!(html.contains("class=\"markdown-alert markdown-alert-caution\""));
+
+        assert!(html.contains(
+            "<p class=\"markdown-alert-title\"><svg class=\"octicon markdown-alert-icon\""
+        ));
+        assert!(html.contains("</svg>Note</p>"));
+        assert!(html.contains("</svg>Tip</p>"));
+        assert!(html.contains("</svg>Important</p>"));
+        assert!(html.contains("</svg>Warning</p>"));
+        assert!(html.contains("</svg>Caution</p>"));
+    }
+
+    #[test]
+    fn keeps_regular_block_quotes_without_alert_chrome() {
+        let renderer = MarkdownRenderer::default();
+        let markdown = "> plain quote";
+        let html = renderer.render(markdown);
+
+        assert!(html.contains(
+            "<blockquote data-line=\"1\"><p data-line=\"1\">plain quote</p></blockquote>"
+        ));
+        assert!(!html.contains("markdown-alert-title"));
     }
 
     #[test]
